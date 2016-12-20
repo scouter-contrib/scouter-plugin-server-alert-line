14083e8b74798a99de620b63a2aca12e1aebbf92
Gunhee Lee <gunlee01@gmail.com>
2016-12-20 22:40:19 +0900

    test

--- src/test/java/scouter/plugin/server/alert/line/LinePluginTest.java
+++ src/test/java/scouter/plugin/server/alert/line/LinePluginTest.java
@@ -17,15 +17,17 @@ import static org.mockito.Mockito.*;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class LinePluginTest {
+    public final static String lineAccessToken = "Wd8jVkD5Fzh7CMl1CTmyOo9ILtWq1MoknQ7kbTMMjQmdU6+cDmfqkwwuE5mB5rLQcFeWCjvjJgnE/MmqT6D+gEsO68vKQh11YygUT7dQmh1JwmWG5mbRqk98Xo1+aBWHllG0AL/6xAp7YMtG9MDVPwdB04t89/1O/w1cDnyilFU=";
+    public static final String lineGroupId = "C0246cfa665d99ec6dde3f12bec77eb4a";
+
     @Mock Configure conf;
     @InjectMocks LinePlugin plugin = new LinePlugin();
 
     @Test
     public void alert() throws Exception {
-        when(conf.getValue("ext_plugin_line_access_token")).thenReturn("Wd8jVkD5Fzh7CMl1CTmyOo9ILtWq1MoknQ7kbTMMjQmdU6+cDmfqkwwuE5mB5rLQcFeWCjvjJgnE/MmqT6D+gEsO68vKQh11YygUT7dQmh1JwmWG5mbRqk98Xo1+aBWHllG0AL/6xAp7YMtG9MDVPwdB04t89/1O/w1cDnyilFU=");
-        when(conf.getValue("ext_plugin_line_group_id")).thenReturn("C0246cfa665d99ec6dde3f12bec77eb4a");
+        when(conf.getValue("ext_plugin_line_access_token")).thenReturn(lineAccessToken);
+        when(conf.getValue("ext_plugin_line_group_id")).thenReturn(lineGroupId);
         when(conf.getBoolean(eq("ext_plugin_line_send_alert"), anyBoolean())).thenReturn(true);
-        //when(conf.getBoolean("ext_plugin_line_send_alert", false)).thenReturn(true);
 
         AlertPack ap = genAlertPack();
         plugin.alert(ap);
